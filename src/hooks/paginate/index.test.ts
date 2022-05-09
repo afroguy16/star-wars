c3e74144d169cbc99ae516f178ee947ae2f6e666
afroguy16 <tega_joe@hotmail.com>
2022-05-09 12:28:16 +0000

    feat [task-3] add config arguments validation for paginate hook

--- src/hooks/paginate/index.test.ts
+++ src/hooks/paginate/index.test.ts
@@ -17,4 +17,22 @@ describe('usePaginate', () => {
         expect(returnedItems).toHaveLength(2)
         expect(returnedItems).toStrictEqual(mockedPaginatedItem)
     })
+
+    it(`should throw an error if config's perPage is less than 1`, () => {
+        expect(() => paginate(results, 0, 2)).toThrowError()
+    })
+
+    it(`should return all the result if config's perPage is more than the length of the arrary given`, () => {
+        const returnedItems = paginate(results, 100, 2)
+        expect(returnedItems).toHaveLength(results.length)
+        expect(returnedItems).toStrictEqual(results)
+    })
+
+    it(`should throw an error if config's currentPage is less than 1`, () => {
+        expect(() => paginate(results, 2, 0)).toThrowError()
+    })
+
+    it(`should throw an error if config's currentPage is more than the available page`, () => {
+        expect(() => paginate(results, 2, 10)).toThrowError()
+    })
 })
--- src/hooks/paginate/index.ts
+++ src/hooks/paginate/index.ts
@@ -12,6 +12,13 @@ function usePaginate(): UsePaginate {
     perPage: number,
     currentPage: number
   ) => {
+    if (perPage < 1) throw new Error('Pagination config`s perPage must be more than 0')
+    if (perPage > arr.length) return arr
+    if (currentPage < 1) throw new Error('Pagination config`s currentPage must be more than 0')
+
+    const availablePages = Math.round((arr.length / perPage))
+    if (currentPage > availablePages) throw new Error('Pagination config`s currentPage is out of bound, enter a lower page number')
+
     const startIndex = perPage * (currentPage - 1);
     const endIndex = startIndex + perPage;
     return arr.slice(startIndex, endIndex);
